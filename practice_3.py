ed44d34a4610951604998fda02ec4bfaa80a2629
张叔阳 <zhangshuyang@gridsum.com>
2018-11-10 13:28:33 +0800

    add plot dynamicly

--- practice_3.py
+++ practice_3.py
@@ -2,7 +2,7 @@
 
 import tensorflow as tf
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
@@ -37,10 +37,24 @@ sess.run(init)
 
 # sess.run(tf.trainable_variables())
 
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()
+# plt.show()
+
 for i in range(1000):
 
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
     if i % 100 == 0:
-        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
 
-    sess.run(tf.global_variables())
+        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+
+        plt.pause(0.1)
+    # sess.run(tf.global_variables())
