98f20be65dc30177b6504729ccdada5eb02720ce
Zhang Shuyang <zhangshuyang@outlook.com>
2018-11-10 14:56:46 +0800

    advanced options of tensorboard

--- practice_4.py
+++ practice_4.py
@@ -4,20 +4,22 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
-    with tf.name_scope("layer"):
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
+    layer_name = f"layer{n_layer}"
+    with tf.name_scope(layer_name):
         with tf.name_scope("weights"):
-
             Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+            tf.summary.histogram(f"{layer_name}/weights", Weights)
         with tf.name_scope("biases"):
             biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+            tf.summary.histogram(f"{layer_name}/biases", biases)
         with tf.name_scope("Wx_plus_b"):
             Wx_plus_b = tf.matmul(inputs, Weights) + biases
         if activation_function is None:
             outputs = Wx_plus_b
         else:
             outputs = activation_function(Wx_plus_b)
-
+        tf.summary.histogram(f"{layer_name}/output", outputs)
         return outputs
 
 
@@ -31,11 +33,12 @@ with tf.name_scope("inputs"):
     ys = tf.placeholder(tf.float32, [None, 1], name="y_input")
 
 
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
-prediction = add_layer(l1, 10, 1, activation_function=None)
+l1 = add_layer(xs, 1, 10, 1, activation_function=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, 2, activation_function=None)
 
 with tf.name_scope("loss"):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+    tf.summary.scalar("loss", loss)
 
 with tf.name_scope("train"):
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
@@ -45,6 +48,7 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
+merged = tf.summary.merge_all()
 writer = tf.summary.FileWriter("logs/", sess.graph)
 # sess.run(tf.trainable_variables())
 
@@ -52,9 +56,10 @@ writer = tf.summary.FileWriter("logs/", sess.graph)
 for i in range(1000):
 
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
-    if i % 100 == 0:
-
+    if i % 50 == 0:
+        result = sess.run(merged, feed_dict={xs: x_data, ys: y_data})
         print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+        writer.add_summary(result, i)
 
     # sess.run(tf.global_variables())
 
